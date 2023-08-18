1e5e270213907006001587b17b50906bf0390986
Sosna123 <olek.arez@gmail.com>
2023-08-18 11:05:07 +0200

    POST fetching works

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // express
 const express = require('express')
 
+// bodyParser
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: true })
+
 // mongoose
 const mongoose = require('mongoose')
 const dbURI = 'mongodb+srv://olekarez:test123@notatki-konto.tvmbsxd.mongodb.net/?retryWrites=true&w=majority'
@@ -29,20 +34,23 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .catch((err) => {console.log(err)});
 
 app.use(morgan('dev'));
+app.use(jsonParser);
+app.use(urlencodedParser);
+app.use(cors(corsOptions));
 
 
 // wysyłanie danych
 
-app.get("/", cors(corsOptions), (req, res) => {
+app.get("/", (req, res) => {
     Note.find().then((data) => res.json(data));
 });
 
-app.get("/:id", cors(corsOptions), (req, res) => {
+app.get("/:id", (req, res) => {
     const { id } = req.params;
     Note.findById(id).then((data) => res.json(data));
 });
 
-app.post('/', cors(corsOptions), (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body)
     const newNote = new Note(req.body)
     newNote.save().then(() => {
@@ -50,7 +58,7 @@ app.post('/', cors(corsOptions), (req, res) => {
     })
 })
 
-app.delete('/:id', cors(corsOptions), (req, res) => {
+app.delete('/:id', (req, res) => {
     const { id } = req.params;
     Note.findByIdAndDelete(id).then(() => {
         res.redirect('/')
