8fa9a1f0d97aaca6a8ca0dab10bb63fdec0395d1
Sosna123 <olek.arez@gmail.com>
2023-09-03 19:25:02 +0200

    Some cleanup after comeback

--- models.js
+++ models.js
@@ -1,30 +1,12 @@
 const mongoose = require('mongoose')
 
 const NoteSchema = new mongoose.Schema({
-    user:{
-        type: String,
-        required: true
-    },
     content:{
-        type: String,
-        required: true
-    },
-})
-
-const UserSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true
-    },
-    password:{
         type: String,
         required: true
     }
 })
 
 const Note = mongoose.model('Note', NoteSchema)
-const User = mongoose.model('User', UserSchema)
 
-module.exports = {
-    Note, User
-};
+module.exports = { Note };
